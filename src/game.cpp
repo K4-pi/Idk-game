9adf1f770e409ecba80979b5451c10d9c1254dbd
K4-pi <Kap1_priv@proton.me>
2025-01-08 13:53:12 +0100

    Player movement

--- src/game.cpp
+++ src/game.cpp
@@ -1,6 +1,5 @@
 #include "raylib.h"
 #include <cmath>
-#include "stdio.h"
 
 typedef struct Player {
     Vector2 position;
@@ -20,32 +19,36 @@ void game(int fps) {
     const int screenWidth = 800;
     const int screenHeight = 450;
 
-    //const float moveSpeed = 15.0;
-
     InitWindow(screenWidth, screenHeight, "Game title");
     SetTargetFPS(fps);
+
+    const float moveSpeed = 1.5f;
 
     Player player = { 0 }; //Initializing all variables
     player.position.x = screenWidth / 2;
     player.position.y = screenHeight / 2;
 
-    float horizontal = 0.0f;
-    float vertical = 0.0f;
+    Vector2 direction = { 0 };
 
     while (!WindowShouldClose()) {
         // Functionality
-        if (IsKeyDown(KEY_RIGHT)) horizontal = 1.0f;
-        if (IsKeyDown(KEY_LEFT)) horizontal = -1.0f;
-        if (IsKeyDown(KEY_UP)) vertical = -1.0f;
-        if (IsKeyDown(KEY_DOWN)) vertical = 1.0f;
+        if (IsKeyDown(KEY_RIGHT)) direction.x = 1.0f;
+        else if (IsKeyDown(KEY_LEFT)) direction.x = -1.0f;
+        else direction.x = 0.0f;
 
-        NormalizeVector2(player.position);
+        if (IsKeyDown(KEY_UP)) direction.y = -1.0f;
+        else if (IsKeyDown(KEY_DOWN)) direction.y = 1.0f;
+        else direction.y = 0.0f;
+
+        direction = NormalizeVector2(direction);
+
+        player.position.x += direction.x * moveSpeed;
+        player.position.y += direction.y * moveSpeed;
 
         // Rendering
         BeginDrawing();
 
             DrawRectangle(player.position.x, player.position.y, 25, 25, RED);
-
             ClearBackground(RAYWHITE);
 
         EndDrawing();
